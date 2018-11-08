05015a789951a6826e2ef5a4a18600a21e688937
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-07 20:22:51 -0500

    Finish adding comments and removing extraneous comments.

--- Assignments/Assignment_1/Assignment_1.playground/Contents.swift
+++ Assignments/Assignment_1/Assignment_1.playground/Contents.swift
@@ -19,31 +19,6 @@
  */
 
 
-/*
-
- Game plan:
- 
- Create a function called loopArray that will accept an int as an input (this input will henceforth be called "loopNum")
- 
- If loopNum = 3...
- * Index 0: 1 => Index 4        // went up by 3
- * Index 2: 3 => Index 0
- 
- Will need to loop from i=0 to i=array.length-1
- 
- Size of array is 5.
- 
- 5-5=0
- 5-4=1
- 5-3=2
- 5-4=1
- 5-5=0
- 
- * i=1:
- 
- */
-
-
 
 import UIKit
 
@@ -51,39 +26,47 @@ var str = "Hello, playground"
 
 //let firstArray: [Int] = [1, 2, 3, 4, 5]
 let firstArray: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// This array will become the looped version of the firstArray.
+// To give this array a length, I filled a number of elements with nil values.
+// This array was made the same length as the firstArray.
+// This array needs an optional because it begins as an array of nil values, but ends as an array with non-nil values.
 var newArray: Array<Int?> = Array(repeating: nil, count: firstArray.count)
-//var newIndex:Int
-
-//func loopArray (loopNum:Int, arrayIn:Array<Int>, arrayOut:Array<Int>) -> Array<Int> {
-////    for item in arrayIn {
-////        if item + loop >= arrayIn.count {
-////
-////        }
-////    }
-//
-//    for var i in 0 ..< arrayIn.count {
-//        if i + loopNum >= arrayIn.count {
-//            let newIndex:Int = abs(arrayIn.count - (i + loopNum))
-//            arrayOut[newIndex] = arrayIn[i]
-//        }
-//    }
-//}
 
 
+
+// This function will "loop" a given array.
+// Input the array into the "arrayIn" parameter.
+// Specify the number of rightward loops in the "loopNum" parameter
+// Since "arrayOut" will be changed, it needs to have the inout parameter.
+// The function will return an array of integers.
 func loopArray(loopNum:Int, arrayIn:Array<Int>, arrayOut:inout Array<Int?>) -> Array<Int?> {
     for var i in 0 ..< arrayIn.count {
+        
+        // if the sum of the element index plus the number of loops is equal to or exceeds the length of "arrayIn," then...
         if i + loopNum >= arrayIn.count {
+            
+            // Calculate the element's proper new index
             var newIndex:Int = abs(arrayIn.count - (i + loopNum))
+            
+            // Store the element in its new index in "arrayOut"
             arrayOut[newIndex] = arrayIn[i]
         }
+            
+        // Otherwise...
         else {
+            
+            // The new index does not need any confusing calculations.
+            // Just advance the index by the number of loops specified.
             arrayOut[i+loopNum] = arrayIn[i]
         }
     }
     
+    // returns the looped array.
     return arrayOut
 }
 
+// Function call
 loopArray(loopNum:3, arrayIn:firstArray, arrayOut: &newArray)
 
 

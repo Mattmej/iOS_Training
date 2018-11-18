e16616126652ad1d00c53ee936021d3baf50e3cc
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-18 17:31:57 -0500

    First die now returns random number.

--- Exercises/Udemy/Dicee/Dicee/ViewController.swift
+++ Exercises/Udemy/Dicee/Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
 
@@ -29,7 +31,9 @@ class ViewController: UIViewController {
         
         print(randomDiceIndex1)
         
-        diceImageView1.image = UIImage(named: "dice2")
+//        diceImageView1.image = UIImage(named: "dice2")
+        
+        diceImageView1.image = (UIImage(named: diceArray[randomDiceIndex1]))
     }
     
     override func viewDidLoad() {
